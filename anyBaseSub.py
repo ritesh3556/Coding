d774ac93ef11f2948dd9614265bf90698695dc68
ritesh3556 <reeteshgupta52270@gmail.com>
2021-08-20 13:42:47 +0530

    anyBaseSub

---
+++ anyBaseSub.py
@@ -0,0 +1,36 @@
+
+
+
+
+def anyBaseSub(base, n1,n2):
+    
+    
+    r=0
+    p=1
+    c=0
+    while(n1 or n2 or c):
+        
+        
+        d1 = (n1%10)
+        d2 = (n2%10)
+        n1//=10
+        n2//=10
+        
+        d2 = d2+c
+        if d2>=d1:
+            
+            c=0
+            d = d2-d1
+        else:
+            c=-1
+            d = d2+base-d1
+        
+        r += d*p
+        p*=10
+    
+    return r
+
+base = int(input())
+n1 = int(input())
+n2 = int(input())
+print(anyBaseSub(base,n1,n2))
